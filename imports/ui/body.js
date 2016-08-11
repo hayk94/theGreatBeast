2bbfe0c2d48ef81732235027a3fe13f45088339c
Hayk <hayk94@gmail.com>
2016-08-11 15:07:45 +0400

    4.2 add event handler for form submit

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -10,3 +10,24 @@ Template.body.helpers({
     return Tasks.find({});
   },
 });
+
+
+Template.body.events({
+  "submit .new-task": function(event){
+     //prevent default submit
+     event.preventDefault();
+     console.log(event);
+     //Get value from form element
+     const target = event.target;
+     const text = target.text.value;
+
+     //insert a task into the Collection
+     Tasks.insert({
+       text,
+       createdAt: new Date(),//current time
+     });
+
+     //clear form
+     target.text.value ='';
+  }
+});

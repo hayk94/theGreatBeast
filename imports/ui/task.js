91a5f984bb7ab0e7630c94fbb8d51e69d7d71bf6
Hayk <hayk94@gmail.com>
2016-08-12 15:28:25 +0400

    making so that only the logged in user mouse is tracked

--- imports/ui/task.js
+++ imports/ui/task.js
@@ -1,8 +1,12 @@
+import { Meteor } from 'meteor/meteor';
+
 import {Template} from 'meteor/templating';
 
 import {Tasks} from '../api/tasks.js';
 
 import './task.html';
+
+
 
 Template.task.helpers({
   isOwner() {
@@ -28,3 +32,64 @@ Template.task.events({
    Meteor.call('tasks.setPrivate', this._id, !this.private);
  }
 });
+
+
+
+
+
+
+
+  Template.task.onCreated(function bodyOnCreated() {
+    // mousetracker
+      // console.log('I log');
+      (function() {
+    //     "use strict";
+        var dot = document.getElementById('dot');
+        // var dot;
+        // Add a dot to follow the cursor
+          // dot = document.createElement('div');
+          // dot.className = "dot";
+          dot.style.left = 0 + "px";
+          dot.style.top = 0 + "px";
+          //dot.style.left = event.pageX + "px";
+          //dot.style.top = event.pageY + "px";
+          // document.body.appendChild(dot);
+
+        document.onmousemove = handleMouseMove;
+        function handleMouseMove(event) {
+          var eventDoc, doc, body, pageX, pageY;
+
+          event = event || window.event; // IE-ism
+
+          // If pageX/Y aren't available and clientX/Y
+          // are, calculate pageX/Y - logic taken from jQuery
+    			// Calculate pageX/Y if missing and clientX/Y available
+          if (event.pageX == null && event.clientX != null) {
+            eventDoc = (event.target && event.target.ownerDocument) || document;
+            doc = eventDoc.documentElement;
+            body = eventDoc.body;
+            if (Meteor.userId()) {
+
+            }
+            event.pageX = event.clientX +
+              (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+              (doc && doc.clientLeft || body && body.clientLeft || 0);
+            event.pageY = event.clientY +
+              (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+              (doc && doc.clientTop  || body && body.clientTop  || 0 );
+          }
+            dot.style.left = event.pageX + "px";
+            dot.style.top = event.pageY + "px";
+
+        }
+      })();//func mousetracker
+
+      // TODO: send the pageX and pageY to the db and then get them from the db and assign to another element
+        // TODO: try with reactive-var first
+          // TODO: Get the user list
+        // TODO: Create a new collection
+        // TODO: send the pageX and pageY
+        // TODO: get the pageX and pageY
+
+      //mouse tracker
+  }); // onCreated
